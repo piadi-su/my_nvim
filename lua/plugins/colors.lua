cfb63cc0d48e9731fd1ff2bf5a945a53415c525c
piadi-su <213212422+piadi-su@users.noreply.github.com>
2026-06-29 00:07:30 +0200

    mod lua line + colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -83,4 +83,39 @@ return {
   --  },
   -- },
 
+  {
+	  "nvim-lualine/lualine.nvim",
+	  dependencies = {
+		  "nvim-tree/nvim-web-devicons",
+	  },
+	  opts = {
+		  options = {
+			  theme = "auto",
+			  icons_enabled = false,
+			  section_separators = "",
+			  component_separators = "",
+			  globalstatus = true,
+		  },
+
+		  sections = {
+			  lualine_a = { "mode" },
+			  lualine_b = { "branch" },
+			  lualine_c = { "filename" },
+			  lualine_x = {},
+			  lualine_y = { "progress" },
+			  lualine_z = { "location" },
+		  },
+
+		  inactive_sections = {
+			  lualine_a = {},
+			  lualine_b = {},
+			  lualine_c = { "filename" },
+			  lualine_x = {},
+			  lualine_y = {},
+			  lualine_z = {},
+		  },
+	  },
+  }
+
+
 }
